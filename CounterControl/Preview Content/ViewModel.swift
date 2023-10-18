2f99feba55afd599810c97181fc26409525d7bd7
miyakiyu <sherry910509@gmail.com>
2023-10-18 22:11:54 +0800

    First version

---
+++ CounterControl/Preview Content/ViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ViewModel.swift
+//  CounterControl
+//
+//  Created by 謝于瑄 on 2023/10/18.
+//
+
+import SwiftUI
+
+struct ViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewModel()
+    }
+}
